d93f443597aaa914ef8a46ba482c7b89101e4628
chris <chrishaig14@gmail.com>
2017-10-21 22:54:17 -0300

    Había quedado mal algo del run_tests...

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -49,6 +49,7 @@ filenames = sys.argv[1:]
 lengths = [5, 10, 25, 50, 75, 100, 200]  # largo de los patrones a testear
 out = open("out.csv", 'w')  # csv output
 import os
+
 for filename in filenames:
     print("\n\n")
     print("File: " + os.path.basename(filename))
@@ -58,7 +59,7 @@ for filename in filenames:
 
     for l in lengths:
         # Patron al azar de longitud l
-        print("Pattern length: " +  str(l))
+        print("Pattern length: " + str(l))
         pos = random.randint(0, len(t1) - 1)
         p = t1[pos:pos + l]
         print("Testing Colussi ...")
@@ -69,8 +70,10 @@ for filename in filenames:
         # uko_time, uko_results = test_uko(filename, p)
         uko_time = 0.0
 
-        result = str(col_results == dc3_results) # En lugar de esto que se fije si uko_results = col_results = dc3_results
+        result = str(
+            col_results == dc3_results)  # En lugar de esto que se fije si uko_results = col_results = dc3_results
 
-        out.write(filename + "\t" + str(len(t1)) + "\t" + str(len(set(t1))) + "\t" + p + "\t" + "{0:.3f}".format(
+        out.write(os.path.basename(filename) + "\t" + str(len(t1)) + "\t" + str(
+            len(set(t1))) + "\t" + p + "\t" + "{0:.3f}".format(
             col_time) + "\t" + "{0:.3f}".format(uko_time) + "\t" + "{0:.3f}".format(
-            dc3_time) + "\t" + str(col_results == dc3_results) + "\t"
+            dc3_time) + "\t" + str(col_results == dc3_results) + "\t")
